e109350278afffb23bf04fb0052fbb99e896dc0a
Marwa <marwa@wias-berlin.de>
2025-08-21 07:37:53 +0200

    DrWatson in separate file

--- scripts/flows_plot.jl
+++ scripts/flows_plot.jl
@@ -17,102 +17,6 @@ using Colors
 using ColorTypes
 #gr()
 
-quickactivate(@__DIR__, "NumCompressibleFlows")
-mkpath(plotsdir("compressible_stokes/convegence_history"))
-
-function filename_plots(data; prefix = "", free_parameter = "")
-    μ = data["μ"] 
-    c = data["c"]
-    γ = data["γ"]
-    stab1 = data["stab1"]
-    stab2 = data["stab2"]
-    ϵ = 1 - stab1[1]
-    α = stab2[1]
-    c1 = stab1[2]
-    c2 = stab2[2]
-    velocitytype = data["velocitytype"]
-    densitytype = data["densitytype"]
-     sname = savename((@dict μ c γ ))
-    sname = "plots/compressible_stokes/convegence_history/" * "ϵ=$(ϵ)_"*"α=$(α)_c1=$(c1)_"*"c2=$(c2)_" * sname * prefix * ".png"
-    return sname
-end
-
-default_args = Dict(
-    # problem parameters
-    "μ" => 1,
-    "λ" => 0,
-    "γ" => 1,
-    "c" => 1,
-    "M" => 1,
-    # solving options
-    "τfac" => 1,
-    "ufac" => 1,
-    "nrefs" => 1,
-    "order" => 1,
-    "pressure_stab" => 0,
-    "bonus_quadorder" => 4,
-    "maxsteps" => 5000,
-    "target_residual" => 1.0e-11,
-    "reconstruct" => true,
-    # data of the problem
-    "velocitytype" => ZeroVelocity,
-    "densitytype" => ExponentialDensity,
-    "convectiontype" => NoConvection,
-    "coriolistype" => NoCoriolis,
-    "eostype" => IdealGasLaw,
-    "gridtype" => Mountain2D,
-    "pressure_in_f" => false,
-    "laplacian_in_rhs" => true,
-    "stab1" => (1-0.1,0),
-    "stab2" => (1.5,0),
-)
-
-function load_data(; kwargs...)
-    data = deepcopy(default_args) 
-    for (k,v) in kwargs 
-        data[String(k)] = v 
-    end
-    return data
-end
-
-
-function plot_convergencehistory(; nrefs = 1:6, Plotter = Plots, force = false, kwargs...)
-
-    data = load_data(; kwargs...)
-    #@show data
-    Results = zeros(Float64, length(nrefs), 5)
-    NDoFs = zeros(Float64, length(nrefs))
-
-    for lvl in nrefs
-        data["nrefs"] = lvl
-        data, ~ = produce_or_load(run_single, data, filename = filename, force = force)
-        NDoFs[lvl] = data["ndofs"]
-        Results[lvl,1] = data["Error(L2,u)"]
-        Results[lvl,2] = data["Error(H1,u)"] 
-        Results[lvl,3] = data["Error(L2,ϱ)"]
-        Results[lvl,4] = data["Error(L2,ϱu)"]
-        Results[lvl,5] = data["nits"]
-
-        print_convergencehistory(NDoFs[1:lvl], Results[1:lvl, :]; X_to_h = X -> X .^ (-1 / 2), ylabels = ["|| u - u_h ||", "|| ∇(u - u_h) ||", "|| ϱ - ϱ_h ||", "|| ϱu - ϱu_h ||", "#its"], xlabel = "ndof")
-    end
-
-    ## plot
-    #Plotter.rc("font", size=20)
-    yticks = [1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1,1e+1,1e+2]
-    xticks = [1e1,1e2,1e3,1e4,1e5]
-    Plotter.plot(; show = true, size = (1000,1000), margin = 1Plots.cm, legendfontsize = 20, tickfontsize = 22, guidefontsize = 26, grid=true)
-    Plotter.plot!(NDoFs, Results[:,2]; xscale = :log10, yscale = :log10, linewidth = 3, marker = :circle, markersize = 5, label = L"|| ∇(\mathbf{u} - \mathbf{u}_h)\,||", grid=true)
-    Plotter.plot!(NDoFs, Results[:,3]; xscale = :log10, yscale = :log10, linewidth = 3, marker = :circle, markersize = 5, label = L"|| {ϱ}-ϱ_h \, ||", grid=true)
-    Plotter.plot!(NDoFs, Results[:,4]; xscale = :log10, yscale = :log10, linewidth = 3, marker = :circle, markersize = 5, label = L"|| {ϱ\mathbf{u}}-ϱ_h \mathbf{u}_h \, ||", grid=true)
-    Plotter.plot!(NDoFs, Results[:,1]; xscale = :log10, yscale = :log10, linewidth = 3, marker = :circle, markersize = 5, label = L"|| \mathbf{u} - \mathbf{u}_h \,||", grid=true)
-    Plotter.plot!(NDoFs, 200*NDoFs.^(-0.5); xscale = :log10, yscale = :log10, linestyle = :dash, linewidth = 3, color = :gray, label = L"\mathcal{O}(h)", grid=true)
-    Plotter.plot!(NDoFs, 200*NDoFs.^(-1.0); xscale = :log10, yscale = :log10, linestyle = :dash, linewidth = 3, color = :gray, label = L"\mathcal{O}(h^2)", grid=true)
-    #Plotter.plot!(NDoFs, 100*NDoFs.^(-1.25); xscale = :log10, yscale = :log10, linestyle = :dash, linewidth = 3, color = :gray, label = L"\mathcal{O}(h^{2.5})", grid=true)
-    
-    Plotter.plot!(; legend = :topright, xtick = xticks, yticks = yticks, ylim = (yticks[1]/2, 2*yticks[end]), xlim = (xticks[1], xticks[end]), xlabel = "ndofs",gridalpha = 0.7,grid=true, background_color_legend = RGBA(1,1,1,0.7))
-    ## save
-    Plotter.savefig(filename_plots(data))
-end
 
 function main(;
     nrefs = 4,
@@ -366,5 +270,6 @@ end
 #Plotter.savefig("DensityStab_S1$(stab1)_S2$(stab2)_(velocitytype)_$(densitytype)_$(eostype)_μ$(μ)_c$(c)_M$(M).png")
 Plotter.savefig("Aconvegence_history_S1$(stab1)_S2$(stab2)_(velocitytype)_$(densitytype)_$(eostype)_μ$(μ)_c$(c)_M$(M).png")
 
-return Results, plt
+# return Results, plt
+return Results
 end
