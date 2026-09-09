c0f2aa9caba7fea6cc18d2ab091bf91919c84fb0
Christian <merdon@edu-099.wias-berlin.de>
2026-09-09 14:51:14 +0200

    updated package versions, explicit imports for Symbolics (to avoid name clash with SymbolicUtils.Unknown)

--- scripts/dr_watson_plot.jl
+++ scripts/dr_watson_plot.jl
@@ -5,7 +5,7 @@ using ExtendableGrids
 using Triangulate
 using SimplexGridFactory
 using GridVisualize
-using Symbolics
+using Symbolics: Symbolics, @variables, build_function
 using LinearAlgebra
 #using Test #hide
 
